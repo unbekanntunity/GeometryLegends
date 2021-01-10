661f7a99cf4f5b7120cbb6c7fc190330db4066f4
unbekanntunity <nguyen3lihn@gmail.com>
2021-01-10 18:34:33 +0100

    Add slide mechanic

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -14,12 +14,34 @@ public class Movement : MonoBehaviour
     [SerializeField]
     private float gravity = -9.81f;
     [SerializeField]
+    private float wallrunGravity;
+    [SerializeField]
     private float jumpHeight = 3f;
 
+    [SerializeField]
+    private float reducedHeight;
     [SerializeField]
     private float slideDurtion = 3f;
     [SerializeField]
     private float time;
+
+    [SerializeField]
+    private LayerMask wall;
+    [SerializeField]
+    private float wallrunForce;
+    [SerializeField]
+    private float maxWallrunTime;
+    [SerializeField]
+    private float maxwallrunSpeed;
+    [SerializeField]
+    private float maxWallRunCameraTilt;
+    [SerializeField]
+    private float wallRunCameraTilt;
+    [SerializeField]
+    private float wallrunDistance;
+
+    public bool isWallRight;
+    public bool isWallLeft;
 
     [SerializeField]
     private GameObject groundCheck;
@@ -29,26 +51,54 @@ public class Movement : MonoBehaviour
     private float groundDistance = 0.4f;
 
     [SerializeField]
-    private float reducedHeight;
+    private KeyCode mouseFocusOn;
+    [SerializeField]
+    private KeyCode mouseFocusOff;
+    [SerializeField]
+    private float mouseSensitivity = 100f;
 
-    public float currentSpeed;
+    private float xRotation = 0f;
+    private Camera camera;
+
+    private float currentSpeed;
     private float originalHeight;
     private bool isGrounded = false;
     private bool isSliding = false;
+    public bool isWallRunning = false;
+
     private Vector3 velocity;
     private CharacterController characterController;
 
     private void Awake()
     {
+        camera = GetComponentInChildren<Camera>();
         characterController = GetComponent<CharacterController>();
         originalHeight = characterController.height;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void Update()
     {
+        MyInput();
+        Look();
+        CheckForWall();
+        WallrunInput();
+        Gravity();
+
+    }
+
+    private void MyInput()
+    {
         isGrounded = Physics.CheckSphere(groundCheck.transform.position, groundDistance, groundMask);
 
-        if (Input.GetKeyDown(slideKey) && Input.GetKey(KeyCode.W) && !isSliding)
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        characterController.Move(move * currentSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(slideKey) && Input.GetKey(KeyCode.W) && !isSliding && isGrounded)
         {
             Sliding();
             Invoke("GetUp", slideDurtion);
@@ -60,21 +110,55 @@ public class Movement : MonoBehaviour
         else if (!Input.GetKey(slideKey))
             currentSpeed = speed;
 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        if (isWallRunning)
+            currentSpeed = maxwallrunSpeed;
 
-        Vector3 move = transform.right * x + transform.forward * z;
-        characterController.Move(move * currentSpeed * Time.deltaTime);
-
-        if (Input.GetKeyDown(jumpKey) && isGrounded)
+        if (Input.GetKeyDown(jumpKey) && (isGrounded || isWallRunning))
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
+    }
+
+    private void Look()
+    {
+        if (Input.GetKeyDown(mouseFocusOn))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else if (Input.GetKeyDown(mouseFocusOff))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        camera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+
+        //Perform the rotations
+        camera.transform.localRotation = Quaternion.Euler(camera.transform.localRotation.x, camera.transform.localRotation.y, wallRunCameraTilt);
+    }
+
+    private void Gravity()
+    {
         if (isGrounded && velocity.y < 0)
             velocity.y = -2f;
 
-        velocity.y += gravity * Time.deltaTime;
-        characterController.Move(velocity * Time.deltaTime);
+        if (!isWallRunning)
+        {
+            velocity.y += gravity * Time.deltaTime;
+            characterController.Move(velocity * Time.deltaTime);
+        }
+        else if (isWallRunning)
+        {
+            velocity.y = wallrunGravity * Time.deltaTime;
+            characterController.Move(velocity * Time.deltaTime);
+        }
     }
 
     private void Sliding()
@@ -86,8 +170,67 @@ public class Movement : MonoBehaviour
 
     private void GetUp()
     {
+        isSliding = false;
         characterController.height = Mathf.Lerp(reducedHeight, originalHeight, time);
         currentSpeed = speed;
-        isSliding = false;
+        if (Input.GetKey(jumpKey))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+
+    private void WallrunInput()
+    {
+        if (Input.GetKey(KeyCode.D) && isWallRight)
+            StartWallrun();
+        if (Input.GetKey(KeyCode.A) && isWallLeft)
+            StartWallrun();
+    }
+
+    private void StartWallrun()
+    {
+        isWallRunning = true;
+        if (isWallLeft && !Input.GetKeyDown(KeyCode.Space))
+            characterController.Move(-transform.right * wallrunForce);
+        else if (isWallRight && !Input.GetKeyDown(KeyCode.Space))
+            characterController.Move(transform.right * wallrunForce);
+    }
+
+    private void StopWallrun()
+    {
+        isWallRunning = false;
+    }
+
+    private void LastJump()
+    {
+        if (Input.GetKey(jumpKey))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+
+    private void CheckForWall()
+    {
+        isWallRight = Physics.Raycast(transform.position, transform.right, wallrunDistance + 0.8f, wall);
+        Debug.DrawRay(transform.position, transform.right, Color.yellow);
+        isWallLeft = Physics.Raycast(transform.position, -transform.right, wallrunDistance, wall);
+        Debug.DrawRay(transform.position, -transform.right, Color.red);
+
+        if (!isWallLeft && !isWallRight)
+        {
+            if (isWallRunning)
+                LastJump();
+            StopWallrun();
+        }
     }
 }
+/*  if (Mathf.Abs(wallRunCameraTilt) < maxWallRunCameraTilt && isWallRunning && isWallRight)
+            wallRunCameraTilt += Time.deltaTime * maxWallRunCameraTilt * 2;
+        if (Mathf.Abs(wallRunCameraTilt) < maxWallRunCameraTilt && isWallRunning && isWallLeft)
+            wallRunCameraTilt -= Time.deltaTime * maxWallRunCameraTilt * 2;
+
+        //Tilts camera back again
+        if (wallRunCameraTilt > 0 && !isWallRight && !isWallLeft)
+            wallRunCameraTilt -= Time.deltaTime * maxWallRunCameraTilt * 2;
+        if (wallRunCameraTilt < 0 && !isWallRight && !isWallLeft)
+            wallRunCameraTilt += Time.deltaTime * maxWallRunCameraTilt * 2;*/
